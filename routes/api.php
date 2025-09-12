56d8a75a9751da3fd07004e37bc1f3758c465efb
MatheusBorsa <matheusalek485@icloud.com>
2025-09-12 20:57:19 -0300

    feat: setting up base endpoints

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,32 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+/*
+POST	/auth/register	Create a new user
+POST	/auth/login	    Authenticate and return token
+POST	/auth/logout	Logout user
+GET	    /auth/me	    Get current logged-in user info
+
+GET	    /clients/{clientId}/users	    List users assigned to this client
+POST	/clients/{clientId}/users	    Attach user to client (with role)
+PATCH	/clients/{clientId}/users/{id}	Update role of a user in this client
+DELETE	/clients/{clientId}/users/{id}	Remove user from client
+
+GET	    /clients	                    List all clients for the logged-in user
+POST	/clients	                    Create a new client
+GET	    /clients/{id}	                Show client details
+PATCH	/clients/{id}	                Update client info
+DELETE	/clients/{id}	                Delete a client
+POST	/clients/{id}/users	            Attach user to client (with role)
+DELETE	/clients/{id}/users/{userId}	Detach user from client
+
+GET	    /clients/{clientId}/tasks	List all tasks for a client
+POST	/clients/{clientId}/tasks	Create a task for a client
+GET	    /tasks/{id}	                Show task details
+PATCH	/tasks/{id}	                Update task (title, desc, due_date, status)
+DELETE	/tasks/{id}	                Delete a task
+PATCH	/tasks/{id}/status	        Update only the status of a task
+
+GET /tasks/{id}/history Get history for a task   
+*/
